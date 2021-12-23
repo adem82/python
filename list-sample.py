aa160294612eec0b4cad53e6419d34ebb49a77ab
jawid <ademiberfi@gmail.com>
2021-12-23 11:25:16 +0200

    first commit

---
+++ list-sample.py
@@ -0,0 +1,16 @@
+import os
+
+list1 = [8, 9, 3, 1, 10]
+list1.reverse()
+print ("the reversed list is", list1)
+
+list2 = [91, 67, 120, 34, 10, 87, 56, 64, 345]
+list2.sort()
+print ("the reversed list is", list2)
+
+list3 = []
+list3 = list1.copy
+print ("list3", list3)
+
+indexvalue = list2[2:6]
+print ("the index values are", indexvalue)
