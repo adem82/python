9326e5eff6b874f5b567d5c5df78a3f47d67813b
adem82 <adem82@users.noreply.github.com>
2021-12-24 22:39:49 +0200

    Update list-sample.py

--- list-sample.py
+++ list-sample.py
@@ -1,10 +1,12 @@
 import os
 
 list1 = [8, 9, 3, 1, 10]
+print ("the list1 is", list1)
 list1.reverse()
 print ("the reversed list is", list1)
 
 list2 = [91, 67, 120, 34, 10, 87, 56, 64, 345]
+print ("the list2 is", list2)
 list2.sort()
 print ("the reversed list is", list2)
 
